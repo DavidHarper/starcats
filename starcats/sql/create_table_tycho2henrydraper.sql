c59c7f6990e107bf4f3fec6bbba8138990a264bb
David Harper <david@obliquity.net>
2019-02-10 19:07:28 +0000

    Add copyright notice.

--- starcats/sql/create_table_tycho2henrydraper.sql
+++ starcats/sql/create_table_tycho2henrydraper.sql
@@ -1,3 +1,25 @@
+-- starcats - a package for loading stars catalogues into a MySQL database.
+--
+-- Copyright (C) 2016-2019 David Harper at obliquity.com
+-- 
+-- This library is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Library General Public
+-- License as published by the Free Software Foundation; either
+-- version 2 of the License, or (at your option) any later version.
+--
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Library General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Library General Public
+-- License along with this library; if not, write to the
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+-- Boston, MA  02111-1307, USA.
+--
+-- See the COPYING file located in the top-level-directory of
+-- the archive of this library for complete text of license.
+
 create table tycho2henrydraper (
        tyc1 smallint unsigned not null,
        tyc2 smallint unsigned not null,
